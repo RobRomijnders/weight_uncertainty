3c7f9c63b1f116389db5510a1595e779ec58be33
robromijnders <romijndersrob@gmail.com>
2018-05-08 10:24:19 +0200

    implement warping

--- weight_uncertainty/util/mutilation.py
+++ weight_uncertainty/util/mutilation.py
@@ -60,3 +60,40 @@ def noise(images, sigma):
 def noise_clip(images, sigma):
     images += np.clip(sigma * np.random.randn(*images.shape), *conf.range)
     return images
+
+
+# SUPER UGLY AND REPEATING CODE
+def warp_cifar(images, warp_value):
+    # TODO merge this function with rotate_cifar()
+    images_out = np.zeros_like(images)
+
+    for n in range(images.shape[0]):
+        imarray = normalize(images[n], reverse=True)
+        im = Image.fromarray(imarray.astype(np.uint8), mode='RGB')
+        im = im.transform((32, 32), Image.AFFINE, data=(1 - warp_value, warp_value, 0, warp_value, 1. + warp_value, 0))
+        images_out[n] = normalize(np.array(im))
+    return images_out
+
+
+def warp(images, warp_value):
+    # TODO merge this function with rotation()
+    if conf.dataset == 'cifar':
+        return warp_cifar(images, warp_value)
+    images_out = np.zeros_like(images)
+    # Check if there is a dimension of unit size
+    try:
+        unit_dim = images.shape.index(1)
+    except ValueError:
+        unit_dim = None
+
+    if unit_dim:
+        images = np.squeeze(images, axis=unit_dim)
+
+    num_batch = images.shape[0]
+    for n in range(num_batch):
+        im = Image.fromarray(images[n])
+        im = im.transform((28, 28), Image.AFFINE, data=(1 - warp_value, warp_value, 0, warp_value, 1. + warp_value, 0))
+        if unit_dim:
+            im = np.expand_dims(np.array(im), axis=unit_dim-1)
+        images_out[n] = im
+    return images_out
