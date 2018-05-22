22da1c6f785ba029894959848a8919ae497ef613
robromijnders <romijndersrob@gmail.com>
2018-05-22 12:22:11 +0200

    play from grid instead of individual images

--- weight_uncertainty/play.py
+++ weight_uncertainty/play.py
@@ -11,12 +11,13 @@ from weight_uncertainty.util.util import calc_risk
 Simple code to read some images and plot a prediction and its uncertainties
 """
 
-
 def read_images():
-    for png in glob(join(conf.input_direc, 'input*.png')):
-        print(png)
-        image = Image.open(png).convert('L')
-        yield (np.array(image).astype(np.float32)-33)/78
+    grid = Image.open('/home/rob/Dropbox/ml_projects/weight_uncertainty/weight_uncertainty/input/grid.png').convert('L')
+    im_array = (np.array(grid).astype(np.float32)-33)/78
+
+    for x_start in [0, 29]:
+        for y_start in [0, 29, 58]:
+            yield im_array[y_start:y_start+28, x_start:x_start+28]
 
 
 batch = np.array(list(read_images()))
@@ -26,7 +27,7 @@ num_images = batch.shape[0]
 model = RestoredModel(conf.restore_direc)
 
 # Make a prediction
-pred = model.sample_prediction(batch, 50)
+pred = model.sample_prediction(batch, 200)
 entropy, mutual_info, sm_var, sm_value, _ = calc_risk(pred)
 decision = np.argmax(np.mean(pred, axis=0), axis=-1)
 
@@ -37,9 +38,9 @@ for n, im in enumerate(batch):
     axarr[n, 0].imshow(im, cmap='gray')
     axarr[n, 0].set_title(f'prediction {decision[n]}')
     axarr[n, 1].imshow(np.ones((28, 28)) * entropy[n], cmap='coolwarm', vmin=0.0, vmax=1.6)
-    axarr[n, 1].set_title(f'Epistemic uncertainty{entropy[n]:7.2f}')
+    axarr[n, 1].set_title(f'Entropy{entropy[n]:7.2f}')
     axarr[n, 2].imshow(np.ones((28, 28)) * mutual_info[n], cmap='coolwarm', vmin=0.0, vmax=1.6)
-    axarr[n, 2].set_title(f'Aleatoric uncertainty{mutual_info[n]:7.3f}')
+    axarr[n, 2].set_title(f'Mutual info {mutual_info[n]:7.3f}')
 
 for axrow in axarr:
     for ax in axrow:
